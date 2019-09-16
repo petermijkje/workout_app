80dbe150de3afbb883e721541c5a4d8cb5fb8891
Peter Sarll <petersarll@gmail.com>
2019-09-16 13:32:08 -0700

    created statements for sign up page to better accept real emails and good passwords

--- workout_app/src/Components/SignUp/SignUp.js
+++ workout_app/src/Components/SignUp/SignUp.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Logo from './logo.png'
 
-function SignUp() {
-  return (
-    <div className="App">
+function validate(email, password) {
+  return {
+    email: email.length === 0,
+    password: password.length === 0
+  };
+}
+
+class SignUp extends Component {
+  constructor(){
+    super()
+    this.state = {
+      email: "",
+      password: "",
+      loggedInUser: {},
+      message: null,
+      errors: [],
+    }
+  }
+
+  emailHandler = e => {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  passwordHandler = e =>{
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  validateFields(email, password) {
+    // we are going to store errors for all fields in a single array
+    const errors = [];
+
+    if (email.length === 0) {
+      errors.push("Email is required");
+    }
+    if (email.split("").filter(x => x === "@").length !== 1) {
+      errors.push("Email should contain a @");
+    }
+    if (email.indexOf(".") === -1) {
+      errors.push("Email should contain at least one dot");
+    }
+    if (password.length === 0) {
+      errors.push("Password is required");
+    }
+
+    if (password.length < 8) {
+      errors.push("Password should be at least 8 characters long");
+    }
+
+    return errors;
+  }
+  canBeSubmitted = () => {
+    const { email, password } = this.state;
+    const errors = validate(email, password);
+    const isDisabled = Object.keys(errors).some(x => errors[x]);
+    return !isDisabled;
+  };
+
+  render(){
+    const { message } = this.state
+    return (
+      <div className="App">
                     <div className="box">
               <div className="sign__in"> 
                 <br />
@@ -18,12 +80,51 @@ function SignUp() {
                <br />
               <form>
                 <label>
-                  <input type="text" name="name" placeholder="Email" className="email"/>
+                  <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="Email" 
+                    className="email"
+                    value= {this.state.email}
+                    onChange={e => this.emailHandler(e)}
+                    required
+                  />
+                              {!this.state.errors ? (
+              <div />
+            ) : (
+              <div>
+                <h1>
+                  {this.state.errors.map(error => {
+                    return (
+                      <h6
+                        style={{
+                          fontSize: "10px",
+                          color: "red",
+                          width: "390px"
+                        }}
+                      >
+                        {error}
+                      </h6>
+                    );
+                  })}
+                </h1>
+              </div>
+            )}
+            {message ? <h6 style={{ color: "red" }}>{message}</h6> : <div />}
                 </label>
                 <br />
                 <hr className="sign__in__hr" />
                 <br />
-                <input type="password" name="name" placeholder="Password" className="password" />
+                <input 
+                  type="password" 
+                  name="name" 
+                  placeholder="Password" 
+                  className="password" 
+                  value={this.state.password}
+                  onChange={e => this.passwordHandler(e)}
+                  required
+                />
+                
                 <br />
                 <hr className="sign__in__hr" />
                 <br />
@@ -35,8 +136,9 @@ function SignUp() {
               <br />
 
               </div>       
-            </div>
+      </div>
   );
+}
 }
 
 export default SignUp;
