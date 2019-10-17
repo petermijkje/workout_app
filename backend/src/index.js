b19d17ed8d24f35edb85d4aee7ef4c60e84f0c41
Peter Sarll <petersarll@gmail.com>
2019-10-17 00:19:15 -0700

    added more

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,31 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
 }
+type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
 `
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
 
 // 2
 const resolvers = {
   Query: {
-    info: () => `This is the app`
+    info: () => `This is the app`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
