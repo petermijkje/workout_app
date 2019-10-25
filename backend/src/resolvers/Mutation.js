cce58cd2dcf4ac1cd1c1043ab56f5f3efdf974bc
Peter Sarll <petersarll@gmail.com>
2019-10-25 16:03:36 -0700

    added post function to mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,6 +2,15 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 
+function post(parent, args, context, info) {
+    const userId = getUserId(context)
+    return context.prisma.createLink({
+      url: args.url,
+      description: args.description,
+      postedBy: { connect: { id: userId } },
+    })
+  }
+
 async function signup(parent, args, context, info) {
     // 1
     const password = await bcrypt.hash(args.password, 10)
