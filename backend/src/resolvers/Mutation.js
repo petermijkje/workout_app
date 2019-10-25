f6414e8b13679c38ed0d0ad6e496e043eeda49da
Peter Sarll <petersarll@gmail.com>
2019-10-25 16:01:19 -0700

    added bcrypt into mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
 async function signup(parent, args, context, info) {
     // 1
     const password = await bcrypt.hash(args.password, 10)
