16ff063dc03bea25a8f1d024d1d1cb1484fdcae2
Peter Sarll <petersarll@gmail.com>
2019-10-29 20:57:28 -0700

    added to mutation.js

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -15,11 +15,9 @@ async function signup(parent, args, context, info) {
     // 1
     const password = await bcrypt.hash(args.password, 10)
     // 2
-    const user = await context.prisma.createUser({ ...args, password })
-  
+    const user = await context.prisma.createUser({ ...args, password })  
     // 3
-    const token = jwt.sign({ userId: user.id }, APP_SECRET)
-  
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)  
     // 4
     return {
       token,
@@ -32,16 +30,13 @@ async function login(parent, args, context, info) {
     const user = await context.prisma.user({ email: args.email })
     if (!user) {
       throw new Error('No such user found')
-    }
-  
+    }  
     // 2
     const valid = await bcrypt.compare(args.password, user.password)
     if (!valid) {
       throw new Error('Invalid password')
-    }
-  
-    const token = jwt.sign({ userId: user.id }, APP_SECRET)
-  
+    }  
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)  
     // 3
     return {
       token,
