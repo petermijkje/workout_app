f68c2012cdd3a63e8dec5d1843d1e93eab6c7c04
Peter Sarll <petersarll@gmail.com>
2019-12-27 18:52:50 -0800

    fixed indentations

--- workout_app/src/App.js
+++ workout_app/src/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import './App.css';
-import SignIn from './Components/SignIn/SignIn.js'
-import SignUp from './Components/SignUp/SignUp.js'
-import { Route } from 'react-router-dom'
-import HomePage from "./Components/HomePage.js"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
+import SignIn from "./Components/SignIn/SignIn.js";
+import SignUp from "./Components/SignUp/SignUp.js";
+import { Route } from "react-router-dom";
+import HomePage from "./Components/HomePage.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // import "./routes"
-
 
 function App() {
   return (
     <div className="App">
-        <HomePage />
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
+      <HomePage />
+      <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/signup" component={SignUp} />
     </div>
   );
 }
--- workout_app/src/Components/MainChart/Chart/BodyPercentageChart.js
+++ workout_app/src/Components/MainChart/Chart/BodyPercentageChart.js
@@ -1,43 +1,36 @@
-import React from 'react';
-import {
-  PieChart, 
-  Pie, 
-  Cell,
-} from 'recharts';
-import './BodyPercentageChart.css'
+import React from "react";
+import { PieChart, Pie, Cell } from "recharts";
+import "./BodyPercentageChart.css";
 
 const data = [
-  { name: 'Group A', value: 400 }, //33.33%
-  { name: 'Group B', value: 300 }, //25%
-  { name: 'Group C', value: 300 }, //25%
-  { name: 'Group D', value: 200 }, //16.67
+  { name: "Group A", value: 400 }, //33.33%
+  { name: "Group B", value: 300 }, //25%
+  { name: "Group C", value: 300 }, //25%
+  { name: "Group D", value: 200 } //16.67
 ];
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
 class BodyPercentageChart extends React.Component {
-  
-render (){
-      return (
-        <div>
-          <PieChart width={500} height={500} onMouseEnter={this.onPieEnter}>
-            <Pie
-              data={data} 
-              cx={120} 
-              cy={200} 
-              innerRadius={80}
-              outerRadius={120} 
-              fill="#8884d8"
-              paddingAngle={5}
-            >
-        	  {
-          	  data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
-            }
-            
-            </Pie>
-            </PieChart>
-            These are your results. <br/>
-
-            </div>
+  render() {
+    return (
+      <div>
+        <PieChart width={500} height={500} onMouseEnter={this.onPieEnter}>
+          <Pie
+            data={data}
+            cx={120}
+            cy={200}
+            innerRadius={80}
+            outerRadius={120}
+            fill="#8884d8"
+            paddingAngle={5}
+          >
+            {data.map((entry, index) => (
+              <Cell fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+        </PieChart>
+        These are your results. <br />
+      </div>
     );
   }
 }
