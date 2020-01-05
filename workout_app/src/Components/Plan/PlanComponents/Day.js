7e19e65c13669ed3fc597e98895db840bc4235a6
Peter Sarll <petersarll@gmail.com>
2020-01-04 19:05:54 -0800

    created a simple bypass to login without having to make additional accounts

--- workout_app/src/Components/Plan/PlanComponents/Day.js
+++ workout_app/src/Components/Plan/PlanComponents/Day.js
@@ -7,21 +7,37 @@ class Day extends Component {
   constructor() {
     super()
     this.state = {
-      workoutDayType: 'chest'
+      workoutDayType: [
+        'chest',
+        'shoulders',
+        'lats',
+        'middle back',
+        'lower back',
+        'quads',
+        'hamstrings',
+        'calves',
+        'abductors',
+        'adductors',
+        'abs',
+        'triceps',
+        'biceps',
+        'forearms'
+      ]
     }
   }
 
   //if the parent renders a specific day, the JSON can
   //match and return specific information pertaining to that day.
   createRandomNumber = () => {
-    const workoutDayType = this.state.workoutDayType
+    // console.log(this.props.workoutDay)
+    // const workoutDayType = this.state.workoutDayType[0]
     let number = ''
     for (
       let number = Math.floor(Math.random() * 906 + 0);
       number < 906;
       number++
     ) {
-      if (exerciseLibrary[number].workoutType !== workoutDayType) {
+      if (exerciseLibrary[number].workoutType !== this.props.workoutDay) {
         continue
         // create an if else statement that says if there are no more types,
         // start from the beginning.
