e50cad970e115fcb26665f0a77d78adc51bea339
Peter Sarll <petersarll@gmail.com>
2020-02-15 14:13:04 -0800

    adding testing component and passing one test rendering component

--- workout_app/src/App.test.js
+++ workout_app/src/App.test.js
@@ -4,28 +4,21 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { MockedProvider } from '@apollo/react-testing'
 import SignIn from './Components/SignIn/SignIn.js'
+import SignUp from './Components/SignUp/SignUp.js'
 import HomePage from './Components/HomePage.js'
 
 import { configure, shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16.1'
 
-configure({ adapter: new Adapter() })
-it('renders without crashing', () => {
-  const div = document.createElement('App')
-  ReactDOM.render(
-    <MockedProvider>
-      <App />
-    </MockedProvider>,
-    div
-  )
-  ReactDOM.unmountComponentAtNode(div)
-})
-
-it('renders children when passed in', () => {
-  const wrapper = shallow(
-    <BrowserRouter>
-      <HomePage />
-    </BrowserRouter>
-  )
-  expect(wrapper.contains(<HomePage />)).to.equal(true)
+describe('testing the App Component', () => {
+  let wrapper
+  it('renders without crashing', () => {
+    const div = document.createElement('App')
+    ReactDOM.render(
+      <MockedProvider>
+        <App />
+      </MockedProvider>,
+      div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+  })
 })
